73868d28161f24d9cf5f156144e6339473a48903
Simon <szhao3454@gmail.com>
2025-03-02 02:32:29 -0800

    new leetcode sout number 6

---
+++ lc002.java
@@ -0,0 +1,12 @@
+public class lc002 {
+    
+    public void output(int a){
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        int a = 6;
+        lc002 two = new lc002();
+        two.output(a);
+    }
+}
